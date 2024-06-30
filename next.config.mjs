50048d6d4f3735fbe83dbfc9386af87e7f5d1ed8
jiyuu-jin <zach@valist.io>
2024-06-29 21:53:28 -0400

    Fix webpack error

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,7 @@
 const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/projects": { page: "/projects" },
-      "/resume": { page: "/resume" },
-      "/contact": { page: "/contact" },
-      "/contribute": { page: "/contribute" },
-    }
-  },
-
-  assetPrefix: '',
-  webpack: (config, { dev }) => {
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    return config
+export default (phase, { defaultConfig }) => {
+  return {
+    assetPrefix: '',
   }
 }

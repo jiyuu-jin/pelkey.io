876284a69eea9bba8a72a035ad3c731164882a07
jiyuu-jin <zach@valist.io>
2024-07-01 16:59:54 -0400

    Fix css imports

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../Home.module.css'
 import Link from 'next/link'
 
 export default function Projects() {
     return (
         <div className= { styles.container } >
-        <h1>Projects < /h1>
-        < /div>
+            <h1>Projects </h1>
+        </div>
     )
 }

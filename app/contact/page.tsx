34db50115b7a3479e6f96b2f3adb9f14f70bde69
jiyuu-jin <zach@valist.io>
2024-07-18 05:29:44 -0400

    Lint route pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../Home.module.css';
+import Link from 'next/link';
 
 export default function Contact() {
     return (
         <div className={styles.container}>
             <h1>Contact</h1>
         </div>
-    )
+    );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../Home.module.css';
+import Link from 'next/link';
 
 export default function Resume() {
     return (
         <div className={styles.container}>
             <h1>Resume</h1>
         </div>
-    )
+    );
 }
